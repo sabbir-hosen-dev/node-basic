0d3d556a34d5789c75102be713184153cd15dd6e
sabbir-hosen-dev <tssabbirhosen@gmai.com>
2024-04-29 23:45:09 +0600

    external module

---
+++ External-module/index.js
@@ -0,0 +1,8 @@
+const getRandomFruitsName = require('random-fruits-name')
+
+// console.log(getRandomFruitsName("en"));
+
+
+const randomMovieNames = require('random-movie-names');
+
+console.log(randomMovieNames(5));
