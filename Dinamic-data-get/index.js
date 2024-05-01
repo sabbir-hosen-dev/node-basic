7ff6e7c0ae54b2611ed8e3325547742c07dfe5e4
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2024-05-02 01:01:52 +0600

    server to gtml page load data

--- Dinamic-data-get/index.js
+++ Dinamic-data-get/index.js
@@ -1,27 +1,35 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const app = express();
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
-
-app.get("/",(req,res) => {
+app.get("/", (req, res) => {
   res.send("sabbir");
-  res.end()
-})
+  res.end();
+});
 
-const users = ["Sabbir","aslam","sani","nazim","tamim","Shohag"]
+const users = ["Sabbir", "aslam", "sani", "nazim", "tamim", "Shohag"];
 
-app.get("/users",(req,res) => {
+app.get("/users", (req, res) => {
   res.send(users);
   res.end();
-})
-app.get("/users/:id",(req,res) => {
+});
+app.get("/users/:id", (req, res) => {
   const id = req.params.id;
-  const name = users[id]
-  res.send({id,name})
-})
+  const name = users[id];
+  res.send({ id, name });
+});
 
-app.listen(3000,() => {
-  console.log(`Server run at http://localhost:3000`)
-})
+app.post("/addUser", (req, res) => {
+    const user = req.body;
+    user.id = 12;
+    res.send(user)
+    res.end()
+});
+
+app.listen(4200, () => {
+  console.log(`Server run at http://localhost:4200`);
+});
+
